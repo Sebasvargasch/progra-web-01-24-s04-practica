592c6ef5bcf6220b9738586bc3d8ac3a4f068508
Sebasvargasch <sebasvargasch@gmail.com>
2024-02-15 19:20:58 -0600

    Update App.js

--- svargas/src/App.js
+++ svargas/src/App.js
@@ -1,26 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState} from "react";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <form action="/my-handling-form-page" method="post">
-  <ul>
-    <li>
-      <label for="name">Nombre:</label>
-      <input type="text" id="name" name="user_name" />
-    </li>
-    <li>
-      <label for="mail">Correo electrónico:</label>
+        <a>
+          Hola mundo
+        </a>
+        <h2>
+      <label for="mail">Email:</label>
       <input type="email" id="mail" name="user_email" />
-    </li>
-    <li>
-      <label for="msg">Mensaje:</label>
-      <textarea id="msg" name="user_message"></textarea>
-    </li>
-  </ul>
-</form>
+    </h2>
+        <h2>
+      <label for="name">password:</label>
+      <input type="text" id="name" name="user_name" />
+    </h2>
+    <br></br>
+    <button onClick="Entregar()">Submit</button>
+
       </header>
     </div>
   );
